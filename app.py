34e889b62b4fc410221ec97223f4ce5e17509138
reyan <reyandres015@gmail.com>
2024-05-29 14:41:38 -0500

    Microservicios añadidos

--- app.py
+++ app.py
@@ -9,60 +9,104 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
 # Simulando una base de datos de usuarios
-users_db = {}
+users_db = []
+totalValores = 0  # Variable para almacenar el total de ventas de todos los usuarios
 
 # Función auxiliar para hashear contraseñas
+
+
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 # Ruta para registrar un nuevo usuario
+
+
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-    
+
     if username in users_db:
         return jsonify({'message': 'User already exists'}), 409
-    
-    users_db[username] = hash_password(password)
+
+    new_user = {
+        'username': username,
+        'password': hash_password(password),
+        'ventas': [],
+        'total': 0,
+    }
+
+    users_db.append(new_user)
     return jsonify({'message': 'User registered successfully'}), 201
 
 # Ruta para autenticar a un usuario y generar un token JWT
+
+
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-    
+
     hashed_password = hash_password(password)
-    
-    if users_db.get(username) != hashed_password:
-        return jsonify({'message': 'Invalid credentials'}), 401
-    
-    token = jwt.encode({
-        'username': username,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
-    }, app.config['SECRET_KEY'], algorithm='HS256')
-    
-    return jsonify({'token': token}), 200
+
+    for user in users_db:
+        if user['username'] == username and user['password'] == hashed_password:
+            token = jwt.encode({
+                'username': username,
+                'password': password,
+                'ventas': user['ventas'],
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+            }, app.config['SECRET_KEY'], algorithm='HS256')
+            return jsonify({'token': token}), 200
+
+    return jsonify({'message': 'Invalid credentials'}), 401
 
 # Ruta para validar el token JWT
+
+
 @app.route('/validate', methods=['POST'])
 def validate():
     token = request.headers.get('Authorization')
     if not token:
         return jsonify({'message': 'Token is missing'}), 401
-    
+
     try:
-        data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        data = jwt.decode(
+            token, app.config['SECRET_KEY'], algorithms=['HS256'])
         return jsonify({'message': 'Token is valid', 'username': data['username']}), 200
     except jwt.ExpiredSignatureError:
         return jsonify({'message': 'Token has expired'}), 401
     except jwt.InvalidTokenError:
         return jsonify({'message': 'Invalid token'}), 401
 
+
+@app.route('/ventas', methods=['POST'])
+def registrarVenta():
+    venta = request.get_json()
+    username = request.headers.get('username')
+
+    for user in users_db:
+        if user['username'] == username:
+            user['ventas'].append(venta)
+            user['total'] += int(venta['valor'])
+            return jsonify({'message': 'Venta registrada'}), 200
+
+    return jsonify({'message': 'Usuario no encontrado'}), 404
+
+
+@app.route('/sumarVentas', methods=['POST'])
+def sumarVenta():
+    global totalValores
+    valor = request.get_json()
+    totalValores += int(valor)
+
+    return jsonify({'message': 'Total actualizado: ', 'total': totalValores}), 200
+
 # Función para manejar conexiones TCP
+
+
 def handle_tcp_connection(client_socket):
     data = client_socket.recv(1024).decode('utf-8')
     print(f"Datos recibidos: {data}")
@@ -70,6 +114,8 @@ def handle_tcp_connection(client_socket):
     client_socket.close()
 
 # Servidor TCP
+
+
 def start_tcp_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('0.0.0.0', 12345))
@@ -79,7 +125,9 @@ def start_tcp_server():
     while True:
         client_socket, addr = server_socket.accept()
         print(f"Conexión establecida con {addr}")
-        threading.Thread(target=handle_tcp_connection, args=(client_socket,)).start()
+        threading.Thread(target=handle_tcp_connection,
+                         args=(client_socket,)).start()
+
 
 # Iniciar el servidor de sockets TCP en un hilo separado
 tcp_thread = threading.Thread(target=start_tcp_server)
--- auth.py
+++ auth.py
@@ -1,36 +1,99 @@
 import requests
 import json
+import threading
+
 
 # URL base de la API
 base_url = 'http://localhost:5000'
 
 # Datos del usuario
 user_data = {
-    'username': 'your_username',
-    'password': 'your_password'
+    'username': '',
+    'password': '',
+    'token': '',
 }
 
 # funcion para registrar un usuario
+
+
 def register_user():
     # set de user_data con input
     user_data['username'] = input('Ingrese un nuevo nombre de usuario: ')
     user_data['password'] = input('Ingrese su  nueva contraseña: ')
     response = requests.post(f'{base_url}/register', json=user_data)
     print('Registro:', response.json())
-    
+
 # funcion para iniciar sesion
+
+
 def login_user():
     # set user_data con input
     user_data['username'] = input('Ingrese su nombre de usuario: ')
     user_data['password'] = input('Ingrese su contraseña: ')
     response = requests.post(f'{base_url}/login', json=user_data)
-    validate_token(response.json().get('token'))
+    user_data['token'] = response.json().get('token')
+    return validate_token(user_data['token'])
 
 # funcion para validar el token
+
+
 def validate_token(token):
     headers = {'Authorization': token}
     response = requests.post(f'{base_url}/validate', headers=headers)
-    print('Validación:', response.json())
+    if response.json().get('message') == 'Invalid token':
+        print('Token inválido')
+        return False
+    elif response.json().get('message') == 'Token has expired':
+        print('Token expirado')
+        return False
+    elif response.json().get('message') == 'Token is valid':
+        return True
+
+
+def postVenta(venta):
+    headers = {'username': user_data['username']}
+    response = requests.post(f'{base_url}/ventas', headers=headers, json=venta)
+    print('Venta registrada:', response.json())
+
+
+def postValorTotal(valor):
+    response = requests.post(f'{base_url}/sumarVentas', json=valor)
+    print('Valor total de venta por usuario:', response.json())
+
+
+def registerVenta(venta):
+    if (validate_token(user_data['token'])):
+        tcp_thread = threading.Thread(target=postVenta, args=(venta,))
+        tcp_thread.daemon = True
+
+        tcp_thread2 = threading.Thread(
+            target=postValorTotal, args=(venta['valor'],))
+        tcp_thread2.daemon = True
+
+        tcp_thread.start()
+        tcp_thread2.start()
+
+        # Esperar a que los hilos terminen
+        tcp_thread.join()
+        tcp_thread2.join()
+
+
+def menu2():
+    while True:
+        print('1. Registrar venta')
+        print('2. Cerrar sesión')
+        option = input('Seleccione una opción: ')
+        if option == '1':
+            venta = {
+                'detalle': str(input('Ingrese el detalle de la venta: ')),
+                'valor': int(input('Ingrese el valor de la venta: ')),
+            }
+            registerVenta(venta)
+        elif option == '2':
+            break
+        else:
+            print('Opción inválida')
+
 
 # menu de opciones
 token = None
@@ -42,7 +105,8 @@ while True:
     if option == '1':
         register_user()
     elif option == '2':
-        token = login_user()
+        if (login_user()):
+            menu2()
     elif option == '3':
         break
     else:
